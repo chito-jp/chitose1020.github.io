4c50b2f0589ac934ae0615465aca1b1ed444b0d8
ちと <chitose.dev@outlook.jp>
2024-10-15 22:39:45 +0900

    updata

--- common/js/common.js
+++ common/js/common.js
@@ -4,7 +4,7 @@ stylesheet.href = "common/css/defer.css";
 document.head.appendChild(stylesheet);
 const header = document.createElement("header");
 header.innerHTML = `<h1><a href="/">Home<a></h1><div class="hamburger"><div class="bar"></div><div class="bar"></div><div class="bar"></div><div>`;
-const hamburger = document.querySelector("header>.hamburger");
+const hamburger = document.querySelector("header .hamburger");
 console.log(hamburger);
 hamburger.addEventListener("click", () => {
     console.log("click!");
