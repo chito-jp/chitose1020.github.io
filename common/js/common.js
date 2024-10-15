a4034d8736a1b1a7b86d79ae71f9c55ec71595bc
ちと <chitose.dev@outlook.jp>
2024-10-15 22:31:50 +0900

    updata

--- common/js/common.js
+++ common/js/common.js
@@ -4,6 +4,10 @@ stylesheet.href = "common/css/defer.css";
 document.head.appendChild(stylesheet);
 const header = document.createElement("header");
 header.innerHTML = `<h1><a href="/">Home<a></h1><div class="hamburger"><div class="bar"></div><div class="bar"></div><div class="bar"></div><div>`;
+const hanburger = document.querySelector("header>.hanburger");
+hanburger.addEventListener("click", () => {
+    console.log("click!");
+});
 document.body.appendChild(header);
 const main = document.createElement("main");
 document.body.appendChild(main);
