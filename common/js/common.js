19f2f68f73e6ad15e5e618ad72e3e6c18101974d
ちと <chitose.dev@outlook.jp>
2024-10-15 21:43:48 +0900

    updata

--- common/js/common.js
+++ common/js/common.js
@@ -3,7 +3,7 @@ stylesheet.rel = "stylesheet";
 stylesheet.href = "common/css/defer.css";
 document.head.appendChild(stylesheet);
 const header = document.createElement("header");
-header.innerHTML = `<div><h1>Home</h1></div><div>🍔<div>`;
+header.innerHTML = `<a><h1>Home</h1></a><div>🍔<div>`;
 document.body.appendChild(header);
 const main = document.createElement("main");
 document.body.appendChild(main);
