214fbda0949a5987a24335e00e20567ade13d430
ちと <chitose.dev@outlook.jp>
2024-10-15 21:02:21 +0900

    updata common.css

--- common/js/common.js
+++ common/js/common.js
@@ -3,6 +3,7 @@ stylesheet.rel = "stylesheet";
 stylesheet.href = "/common/css/common.css";
 document.head.appendChild(stylesheet);
 const header = document.createElement("header");
+header.innerHTML = `<div><h1>Home</h1></div>`;
 document.body.appendChild(header);
 const main = document.createElement("main");
 document.body.appendChild(main);
