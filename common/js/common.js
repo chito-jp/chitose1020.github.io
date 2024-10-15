a912dffef3076c8c9b00e9f8ba7461d1388a5406
ちと <chitose.dev@outlook.jp>
2024-10-15 19:35:02 +0900

    updata

--- common/js/common.js
+++ common/js/common.js
@@ -4,3 +4,5 @@ stylesheet.href = "/common/css/common.css";
 document.head.appendChild(stylesheet);
 const header = document.createElement("header");
 document.body.appendChild(header);
+const main = document.createElement("main");
+document.body.appendChild(main);
