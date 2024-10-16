8a925551dc527d58556f7b6f03f56c8d3f493b90
ちと <chitose.dev@outlook.jp>
2024-10-17 07:52:24 +0900

    updata

--- common/js/common.js
+++ common/js/common.js
@@ -6,7 +6,7 @@ const header = document.createElement("header");
 header.innerHTML = `<a href="/">Home</a><div class="hamburger"><div class="bar"></div><div class="bar"></div><div class="bar"></div></div><div class="menu"><ul><li><a>Home</a></li></ul></div>`;
 document.body.appendChild(header);
 const hamburger = document.querySelector("header .hamburger");
-const hamburgerMenu = document.getElementById("menu");
+const hamburgerMenu = document.querySelector("header .menu");
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("open");
   hamburgerMenu.classList.toggle("open");
