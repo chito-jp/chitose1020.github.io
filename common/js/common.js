85332ca5e0b3c434f5d641301dde0500a5c86ebc
ちと <chitose.dev@outlook.jp>
2024-10-16 21:24:26 +0900

    updata

--- common/js/common.js
+++ common/js/common.js
@@ -3,7 +3,7 @@ stylesheet.rel = "stylesheet";
 stylesheet.href = "common/css/defer.css";
 document.head.appendChild(stylesheet);
 const header = document.createElement("header");
-header.innerHTML = `<a href="/">Home</a><div class="hamburger"><div class="bar"></div><div class="bar"></div><div class="bar"></div></div><div class="menu"><ul><li><a href="/">home</a></li></ul></div>`;
+header.innerHTML = `<a href="/">Home</a><div class="hamburger"><div class="bar"></div><div class="bar"></div><div class="bar"></div></div>`;
 document.body.appendChild(header);
 const hamburger = document.querySelector("header .hamburger");
 console.log(hamburger);
