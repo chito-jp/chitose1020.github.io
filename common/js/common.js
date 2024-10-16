13d15095e3050b47d031caa42e1d90173317a9fe
ちと <chitose.dev@outlook.jp>
2024-10-17 07:49:52 +0900

    updata

--- common/js/common.js
+++ common/js/common.js
@@ -6,9 +6,10 @@ const header = document.createElement("header");
 header.innerHTML = `<a href="/">Home</a><div class="hamburger"><div class="bar"></div><div class="bar"></div><div class="bar"></div></div><div class="menu"><ul><li><a>Home</a></li></ul></div>`;
 document.body.appendChild(header);
 const hamburger = document.querySelector("header .hamburger");
-console.log(hamburger);
+const hamburgerMenu = document.getElementById("menu");
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("open");
+  hamburgerMenu.classList.toggle("open");
 });
 const main = document.createElement("main");
 document.body.appendChild(main);
